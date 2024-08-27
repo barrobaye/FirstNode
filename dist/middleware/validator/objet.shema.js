3fa105191e4f7ef7108acab1508958b43f7c26f8
barrobaye <bayemamourbarro01@gmail.com>
2024-08-27 13:19:01 +0000

    at 13h

--- dist/middleware/validator/objet.shema.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_shema_1 = require("./client.shema");
-const shema = {
-    "post/api/v1/clients": client_shema_1.clientPostShema,
-    "post/api/v1/article": client_shema_1.clientPostShema
-};
